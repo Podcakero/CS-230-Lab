b2a8730bf5bffd8b30c8cb5c61d8b32e2c9b8f00
Podcakero <raw0047@mix.wvu.edu>
2021-02-05 14:08:00 -0500

    Added photos. Added css files

---
+++ index.php
@@ -0,0 +1,81 @@
+<main>
+    <link rel="stylesheet" href="css\about.css">
+
+    <div class="about">
+        <h1>
+            About Us
+        </h1>
+        <p>
+            iujedwsgfiusiufjgheiusafiu
+        </p>
+    </div>
+    <div class=row>
+        <div class="column">
+            <div class="card">
+                <img src="images/bingus.jpg">
+                <div class="container">
+                    <h2>
+                        CEO
+                    </h2>
+                    <p>
+                        Bingus
+                    </p>
+                    <p>
+                        Email: the_bingus@bingus.com
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <div class="column">
+            <div class="card">
+                <img src="images/floppa.jpg">
+                <div class="container">
+                    <h2>
+                        CFO
+                    </h2>
+                    <p>
+                        Floppa
+                    </p>
+                    <p>
+                        Email: the_floppa@bingus.com
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <div class="column">
+            <div class="card">
+                <img src="images/bongus.png">
+                <div class="container">
+                    <h2>
+                        CTO
+                    </h2>
+                    <p>
+                        Bongus
+                    </p>
+                    <p>
+                        Email: the_bongus@bingus.com
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <div class="column">
+            <div class="card">
+                <img src="images/bunger.webp">
+                <div class="container">
+                    <h2>
+                        Lead Marketing
+                    </h2>
+                    <p>
+                        Bunger
+                    </p>
+                    <p>
+                        Email: the_bunger@bingus.com
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
