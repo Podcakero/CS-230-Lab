b852d1eaac3965bf2277917bafe98bfc2ea5513b
Podcakero <raw0047@mix.wvu.edu>
2021-02-05 20:42:43 -0500

    Added more CSS styling

--- index.php
+++ index.php
@@ -5,13 +5,13 @@
         <h1>
             About Us
         </h1>
-        <p>
-            iujedwsgfiusiufjgheiusafiu
+        <p class="about-text">
+            Bingus Enterprises is an innovative social media engagement and marketing platform designed to help guide you through the tangled mess of the Web. Our clients consist of tiktok stars, news outlets, and Hollywoord Actors.
         </p>
     </div>
     <div class=row>
         <div class="column">
-            <div class="card">
+            <div class="card" id="CEO">
                 <img src="images/bingus.jpg">
                 <div class="container">
                     <h2>
@@ -23,12 +23,20 @@
                     <p>
                         Email: the_bingus@bingus.com
                     </p>
+                    <p>
+                        <h2>
+                            About Bingus
+                        </h2>
+                        <p>
+                            Bingus started his empire on the Internet. Becoming a Meme Sensation at the age of 2 years old, Bingus has helped numerous clients achieve success. And now he leads the charge in our innovative approach to marketing in a web 4.0 world
+                        </p>
+                    </p>
                 </div>
             </div>
         </div>
 
         <div class="column">
-            <div class="card">
+            <div class="card" id="CFO">
                 <img src="images/floppa.jpg">
                 <div class="container">
                     <h2>
@@ -40,12 +48,20 @@
                     <p>
                         Email: the_floppa@bingus.com
                     </p>
+                    <p>
+                        <h2>
+                            About Floppa
+                        </h2>
+                        <p>
+                            Floppa has been in the financial sector since the very beginning. The son of  billionaire philanthroprists Bill and Melinda Gates, Floppa has useeeed his smarts and financial backing to revolutionise digital commerce.
+                        </p>
+                    </p>
                 </div>
             </div>
         </div>
 
         <div class="column">
-            <div class="card">
+            <div class="card" id="CTO">
                 <img src="images/bongus.png">
                 <div class="container">
                     <h2>
@@ -57,12 +73,20 @@
                     <p>
                         Email: the_bongus@bingus.com
                     </p>
+                    <p>
+                        <h2>
+                            About Bongus
+                        </h2>
+                        <p>
+                            Bongus is the younger sibling of Bingus, but don't discount him. An excellent coder, Bongus has worked for the likes of NASA, SpaceX, Facebook, and Amazon revolutionizing their cloud and customer infrastructure. Now, Bongus creates user-specfic Web 4.0 Applications for clients to help boost their Social Media presence.
+                        </p>
+                    </p>
                 </div>
             </div>
         </div>
 
         <div class="column">
-            <div class="card">
+            <div class="card" id="LM">
                 <img src="images/bunger.webp">
                 <div class="container">
                     <h2>
@@ -73,6 +97,14 @@
                     </p>
                     <p>
                         Email: the_bunger@bingus.com
+                    </p>
+                    <p>
+                        <h2>
+                            About Bingus
+                        </h2>
+                        <p>
+                            Bunger, like Bingus, became an Internet Sensation at a very young age. Using his position on the Internet, Bunger has helped revolutionze the marketing strategies of Walmart, Kroger, 7-Eleven, and Oracle. Now, Bunger offers his services to rising Social Media stars to help them utilise their talent and traction.
+                        </p>
                     </p>
                 </div>
             </div>
