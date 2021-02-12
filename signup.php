9eba5d242935ca0b0faab99c0fae6ad93df5d434
Podcakero <raw0047@mix.wvu.edu>
2021-02-12 15:45:44 -0500

    Finished signup-helper.php and completed Lab 3

--- signup.php
+++ signup.php
@@ -3,34 +3,59 @@ require "includes/header.php"
 ?>
 
 <main>
-    <form class="form-signin">
-        <img class="mb-4" src="https://getbootstrap.com/docs/4.0/assets/brand/bootstrap-solid.svg" alt="" width="72" height="72">
-        <h1 class="h3 mb-3 font-weight-normal">
-            Please sign in
-        </h1>
+    <link rel="stylesheet" href="../css/signup.css">
 
-        <label for="inputEmail" class="sr-only">
-            Email address
-        </label>
-        <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
-        
-        <label for="inputPassword" class="sr-only">
-            Password
-        </label>
-        <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
-        
-        <div class="checkbox mb-3">
-            <label>
-                <input type="checkbox" value="remember-me"> Remember me
-            </label>
+    <div class="bg-cover">
+        <div class="h-100 container center-me">
+            <div class="my-auto">
+                <div class="signup-form">
+
+                    <form action="includes/signup-helper.php" method="post">
+
+                        <h1 class="h3 mb-3 font-weight-normal">
+                            Please sign up
+                        </h1>
+
+                        <p class="hint-text">
+                            Create Your Account!
+                        </p>
+
+                        <div class="form-group">
+                            <div class="row">
+                                <div class="col">
+                                    <input type="text" class="form-control" name="fname" placeholder="First name" required autofocus>
+                                </div>
+                                <div class="col">
+                                    <input type="text" class="form-control" name="lname" placeholder="Last name" required>
+                                </div>
+                            </div>
+                        </div>
+
+                        <input type="text" class="form-control" name="uname" placeholder="Username" required>
+
+                        <input type="email" id="inputEmail" class="form-control" name="email" placeholder="Email address" required>
+
+                        <div class="formgroup">
+                            <div class="row">
+                                <div class="col">
+                                    <input type="password" id="inputPassword" class="form-control" name="pwd" placeholder="Password" required>
+                                </div>
+                                <div class="col">
+                                    <input type="password" id="inputPassword" class="form-control" name="con_pwd" placeholder="Confirm Password" required>
+                                </div>
+                            </div>
+                        </div>
+
+                        <button class="btn btn-lg btn-outline-primary btn-block" name="signup-submit" type="submit">
+                            Sign in
+                        </button>
+
+                        <p class="mt-5 mb-3 text-muted">
+                            &copy; 2021
+                        </p>
+                    </form>
+                </div>
+            </div>
         </div>
-
-        <button class="btn btn-lg btn-primary btn-block" type="submit">
-            Sign in
-        </button>
-
-        <p class="mt-5 mb-3 text-muted">
-            &copy; 2017-2018
-        </p>
-    </form>
+    </div>
 </main>
