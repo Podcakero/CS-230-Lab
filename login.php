de79d288b09d75917d568558cff70d7b0c441ad7
Podcakero <raw0047@mix.wvu.edu>
2021-02-18 13:25:36 -0500

    Fix sizing of images

--- login.php
+++ login.php
@@ -15,13 +15,13 @@ require 'includes/header.php';
                 </ol>
                 <div class="carousel-inner">
                     <div class="carousel-item active">
-                        <img class="d-block w-100 c_image" src="images/login_1.jpg" alt="First slide">
+                        <img class="d-block mx-auto c_image" src="images/login_1.jpg" alt="First slide">
                     </div>
                     <div class="carousel-item">
-                        <img class="d-block w-100 c_image" src="images/login_2.jpg" alt="Second slide">
+                        <img class="d-block mx-auto c_image" src="images/login_2.jpg" alt="Second slide">
                     </div>
                     <div class="carousel-item">
-                        <img class="d-block w-100 c_image" src="images/login_3.jpg" alt="Third slide">
+                        <img class="d-block mx-auto c_image" src="images/login_3.jpg" alt="Third slide">
                     </div>
                 </div>
                 <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
