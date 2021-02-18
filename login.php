249ae71387253819e8937c60968f693b065f336d
Podcakero <raw0047@mix.wvu.edu>
2021-02-18 13:20:57 -0500

    Fix formatting of CSS. Add login forms.

--- login.php
+++ login.php
@@ -15,13 +15,13 @@ require 'includes/header.php';
                 </ol>
                 <div class="carousel-inner">
                     <div class="carousel-item active">
-                    <img class="d-block w-100" src="images/login_1.jpg" alt="First slide">
+                        <img class="d-block w-100 c_image" src="images/login_1.jpg" alt="First slide">
                     </div>
                     <div class="carousel-item">
-                    <img class="d-block w-100" src="images/login_2.jpg" alt="Second slide">
+                        <img class="d-block w-100 c_image" src="images/login_2.jpg" alt="Second slide">
                     </div>
                     <div class="carousel-item">
-                    <img class="d-block w-100" src="images/login_3.jpg" alt="Third slide">
+                        <img class="d-block w-100 c_image" src="images/login_3.jpg" alt="Third slide">
                     </div>
                 </div>
                 <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
@@ -36,6 +36,26 @@ require 'includes/header.php';
         </div>
 
         <div class="h-40 center-me">
+            <div class="my-auto">
+                <form class="form-signin" action="includes/login-helper.php" method="post">
+
+                    <h1 class="h3 mb-3 font-weight-normal">
+                        Login
+                    </h1>
+
+                    <input type="text" class="form-control" name="uname-email" placeholder="Username/Email" required>
+
+                    <input type="password" id="inputPassword" class="form-control" name="pwd" placeholder="Password" required>
+
+                    <button class="btn btn-lg btn-outline-primary btn-block" name="signup-submit" type="submit">
+                        Sign in
+                    </button>
+
+                    <p class="mt-5 mb-3 text-muted">
+                        &copy; 2021
+                    </p>
+                </form>
+            </div>
         </div>
     </div>
 </main>
