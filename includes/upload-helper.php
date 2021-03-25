75f1e70376fd82bd55308fed2842650d782173ac
test <test@gmail.com>
2021-03-25 15:50:30 -0400

    Fix profile path

--- includes/upload-helper.php
+++ includes/upload-helper.php
@@ -37,7 +37,7 @@ if (isset($_POST['prof-submit']))
     else
     {
         $new_name = uniqid('', true).".".$ext;
-        $destination = '../profiles/'.$new_name;
+        $destination = 'profiles/'.$new_name;
         $sql = "UPDATE profiles SET profpic = '$destination' WHERE uname = '$uname'";
 
         mysqli_query($conn, $sql);
